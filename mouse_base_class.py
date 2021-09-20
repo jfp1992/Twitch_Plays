51de1fb2353c7bdd6252ea955bef85bfde312927
Jfp1992 <jfp1992@live.co.uk>
2021-09-20 06:26:35 +0100

    Added mouse_base_class to handle mouse actions. # TODO: drag and drop

---
+++ mouse_base_class.py
@@ -0,0 +1,39 @@
+import time
+
+import pyautogui
+from pynput.mouse import Button, Controller
+from pynput.mouse import Events
+
+mouse = Controller()
+scroll = Events()
+# TODO: drag and drop
+
+
+class Mouse:
+    def __init_(self):
+        pass
+
+    def left_click(self, delay=0.05):
+        mouse.press(Button.left)
+        time.sleep(delay)
+        mouse.release(Button.left)
+
+    def right_click(self, delay=0.05):
+        mouse.press(Button.right)
+        time.sleep(delay)
+        mouse.release(Button.right)
+
+    def middle_click(self, delay=0.05):
+        mouse.press(Button.middle)
+        time.sleep(delay)
+        mouse.release(Button.middle)
+
+    def scroll_up(self, count, delay=0.05):
+        for i in range(count):
+            mouse.scroll(0, 1)
+            time.sleep(delay)
+
+    def scroll_down(self, count, delay=0.05):
+        for i in range(count):
+            mouse.scroll(0, -1)
+            time.sleep(delay)
