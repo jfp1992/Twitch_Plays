d57be2be9a4d6770f28724d3f74b84e02b8d4eb6
Jfp1992 <jfp1992@live.co.uk>
2022-03-27 09:10:24 +0100

    Added move and drag events for mouse

--- mouse_base_class.py
+++ mouse_base_class.py
@@ -1,5 +1,6 @@
 import time
 
+import pyautogui
 from pynput.mouse import Button, Controller
 from pynput.mouse import Events
 
@@ -9,38 +10,42 @@ scroll = Events()
 
 
 class Mouse:
-    def __init_(self):
-        pass
+    def left_click_hold(self):
+        mouse.press(Button.left)
 
-    def left_click(self, delay=0.05):
+    def left_click_release(self):
+        mouse.release(Button.left)
+
+    def left_click(self, delay=0):
         mouse.press(Button.left)
         time.sleep(delay)
         mouse.release(Button.left)
 
-    def right_click(self, delay=0.05):
+    def right_click(self, delay=0):
         mouse.press(Button.right)
         time.sleep(delay)
         mouse.release(Button.right)
 
-    def middle_click(self, delay=0.05):
+    def middle_click(self, delay=0):
         mouse.press(Button.middle)
         time.sleep(delay)
         mouse.release(Button.middle)
 
-    def scroll_up(self, count, delay=0.05):
+    def scroll_up(self, count, delay=0):
         for i in range(count):
             mouse.scroll(0, 1)
             time.sleep(delay)
 
-    def scroll_down(self, count, delay=0.05):
+    def scroll_down(self, count, delay=0):
         for i in range(count):
             mouse.scroll(0, -1)
             time.sleep(delay)
 
-# WIP
-    # def move(self, to_x, to_y):
-    #     pyautogui.moveTo(to_x, to_y)
-    #
-    # def drag(self, from_x, from_y, to_x, to_y):
-    #
-    #     self.left_click()
+    def move(self, to_x, to_y):
+        pyautogui.moveTo(to_x, to_y)
+
+    def drag(self, to_x, to_y):
+        self.left_click_hold()
+        self.move(to_x, to_y)
+        self.left_click_release()
+
